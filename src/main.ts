37c59e1fb5a99c293b718757ad4eedb160e36f97
Arda Xi <arda@ardaxi.com>
2017-07-08 14:53:18 +0200

    Add move table

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import { h, init } from 'snabbdom';
 import { VNode } from 'snabbdom/vnode';
+import { Chess } from 'chess.js';
 import { Chessground } from 'chessground';
 import { Api } from 'chessground/api';
+import { Color } from 'chessground/types';
 import klass from 'snabbdom/modules/class';
 import attributes from 'snabbdom/modules/attributes';
 import listeners from 'snabbdom/modules/eventlisteners';
 import style from 'snabbdom/modules/style';
+import * as chunk from 'array.chunk';
 
-export function run(element: Element) {
+export function run(board: Element, table: Element) {
   const patch = init([klass, attributes, listeners, style]);
+  const chess = new Chess();
 
   let cg: Api, vnode: VNode;
+  let rerenderTable;
 
   function rerender() {
-    vnode = patch(vnode || element, render());
+    vnode = patch(vnode || board, renderBoard());
   }
 
   function drawBoard(vnode: VNode) {
@@ -21,12 +26,22 @@ export function run(element: Element) {
       cg.redrawAll();
     } else {
       const el = vnode.elm as HTMLElement;
-      cg = Chessground(el);
+      cg = Chessground(el, {
+        animation: { duration: 1000 },
+        movable: {
+          color: 'white',
+          free: false,
+          dests: toDests(chess)
+        },
+      });
+      cg.set({
+        movable: { events: { after: playerMove(cg, chess, rerenderTable) } }
+      });
       window['cg'] = cg;
     }
   }
 
-  function render() {
+  function renderBoard() {
     return h('div#crowdchess', h('div.blue.merida', 
       h('div.cg-board-wrap', {
         style: {
@@ -40,5 +55,54 @@ export function run(element: Element) {
       })));
   }
 
+  rerenderTable = runTable(chess, table);
+
   rerender();
+}
+
+function runTable(chess: any, element: Element) {
+  const patch = init([klass, attributes, listeners, style]);
+  let vnode: VNode;
+
+  function rerender() {
+    vnode = patch(vnode || element, renderTable());
+  }
+
+  function renderTable() {
+    const table = chunk(chess.history(), 2).map(([w, b], n) => h('tr', [
+      h('th', n+1),
+      h('td', w),
+      h('td', b || '...')
+    ]));
+
+    return h('table', table);
+  }
+
+  return rerender;
+}
+
+function toDests(chess: any) {
+  const dests = {};
+  chess.SQUARES.forEach(s => {
+    const ms = chess.moves({square: s, verbose: true});
+    if (ms.length) dests[s] = ms.map(m => m.to);
+  });
+  return dests;
+}
+
+function playerMove(cg: Api, chess, cb) {
+  return (orig, dest) => {
+    chess.move({from: orig, to: dest});
+    let turnColor: Color = (chess.turn() === 'w') ? 'white' : 'black';
+    cg.set({
+      turnColor: turnColor,
+      movable: {
+        color: turnColor,
+        dests: toDests(chess)
+      },
+      viewOnly: false
+    });
+    setTimeout(cg.toggleOrientation, 1000);
+    cb();
+  };
 }
